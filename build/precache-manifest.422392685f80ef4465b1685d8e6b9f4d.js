b264b9e39e01f6926762a00c9f4b0866824b9fec
Frank <frank.mailservices@gmail.com>
2020-01-08 23:35:42 +0530

    latest

---
+++ build/precache-manifest.422392685f80ef4465b1685d8e6b9f4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f50ae3cb6c3d621b75b6102a8a99ac4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c35889a5dd5f83369c28",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "202af5cb3a5e148dab2a",
+    "url": "/static/js/2.4413c440.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4413c440.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c35889a5dd5f83369c28",
+    "url": "/static/js/main.d693b00e.chunk.js"
+  },
+  {
+    "revision": "f3f5290baee2c8ad6d1a",
+    "url": "/static/js/runtime-main.fec439dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
